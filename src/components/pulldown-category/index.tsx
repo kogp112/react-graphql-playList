1b007e8dd4de3165d70fa387f361af8c0ee83029
kanami.ogawa <gapkana@gmail.com>
2020-02-12 22:13:40 +0900

    modify setting datas in pulldown option

--- src/components/pulldown-category/index.tsx
+++ src/components/pulldown-category/index.tsx
@@ -3,47 +3,6 @@ import SelectedCategory from '../selected-category'
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 
-export default function PulldownCategory() {
-    // Update the document title using the browser API
-    React.useEffect(() => {
-        fetch(url, opts)
-        .then((response) => response.json())
-        .then((responseJson) => {
-            console.log(responseJson);
-        })
-        .catch((error) =>{
-          console.error(error);
-        });
-    });
-    
-    const [selectValues, setSelectValues] = React.useState<string[]>([])
-    
-    const handleChange = (event: React.ChangeEvent<{value: string[]}>) => {
-        event.preventDefault()
-        setSelectValues(event.target.value)
-    }
-    
-    return (
-        <div>
-            <Select name="category" onChange={handleChange} value={selectValues} multiple>
-                {categories.map(category => (
-                    <MenuItem key={category} value={category} >
-                        {category}
-                    </MenuItem>
-                ))}
-            </Select>
-            <SelectedCategory category={selectValues} />
-        </div>
-    )
-}
-
-const categories = [
-    'Rock',
-    'EDM',
-    'House',
-    'Techno'
-];
-
 const query = `
   query {
     reddits {
@@ -60,4 +19,39 @@ const opts = {
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({ query })
 };
+
+export default function PulldownCategory() {
+    React.useEffect(() => {
+        fetch(url, opts)
+        .then((response) => response.json())
+        .then((responseJson) => {
+            setCategories(responseJson.data.reddits);
+        })
+        .catch((error) =>{
+          console.error(error);
+        });
+    });
+    
+    const [selectValues, setSelectValues] = React.useState<string[]>([])
+    const [categories, setCategories] = React.useState<Object>([])
+
+    const handleChange = (event: React.ChangeEvent<{value: string[]}>) => {
+        event.preventDefault()
+        setSelectValues(event.target.value)
+    }
+    
+    return (
+        <div>
+            <Select name="category" onChange={handleChange} value={selectValues} multiple>
+                {Object.keys(categories).map(index => (
+                    <MenuItem key={index} value={categories[index].title} >
+                        {categories[index].title}
+                    </MenuItem>
+                ))}
+            </Select>
+            <SelectedCategory category={selectValues} />
+        </div>
+    )
+}
+
 
