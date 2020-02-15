e00fe05d02a081753c5f24e476b911e361b56ab5
kanami.ogawa <gapkana@gmail.com>
2020-02-15 16:52:40 +0900

    modify pulldown component to treeview

--- src/components/pulldown-category/index.tsx
+++ src/components/pulldown-category/index.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react'
 import SelectedCategory from '../selected-category'
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
+//import Select from '@material-ui/core/Select';
+//import MenuItem from '@material-ui/core/MenuItem';
+import { TreeItem,TreeView } from '@material-ui/lab';
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+
 
 const query = `
   query {
@@ -18,7 +22,43 @@ const opts = {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({ query })
-};
+}
+
+interface Category {
+    title: string,
+    categories: Array<any>
+}
+
+type PropsType = {
+    subgenres: Category[],
+    subgenre: Array<any>
+}
+
+const GenreTreeView = (props: PropsType) => {
+   console.log('subgenres is',props.subgenres)
+    return (
+      <TreeView
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+      >
+        <SubGenreTreeItem subgenre={props.subgenres} />
+      </TreeView>
+    );
+}
+
+const SubGenreTreeItem = (props: PropsType) => {
+  console.log('subgenre is',props.subgenre.subGenreUrlList)
+    return (
+      <TreeItem nodeId={"1"} label={props.subgenre.title}>
+        {Object.keys(props.subgenre.subGenreUrlList).map((category: string, index: number) => (
+          <TreeItem
+            nodeId={String(index)}
+            label={props.subgenre.subGenreUrlList[index]}
+          />
+        ))}
+      </TreeItem>
+    )
+}
 
 export default function PulldownCategory() {
     React.useEffect(() => {
@@ -27,7 +67,7 @@ export default function PulldownCategory() {
         .then((responseJson) => {
             setCategories(responseJson.data.reddits);
         })
-        .catch((error) =>{
+        .catch((error) => {
           console.error(error);
         });
     }, []);
@@ -41,22 +81,25 @@ export default function PulldownCategory() {
     }
     
     return (
-        <div>
-            <Select name="category" onChange={handleChange} value={selectValues} multiple>
-                {Object.keys(categories).map((category: string, index: number) => (
-                    <MenuItem key={index} value={categories[index].title} >
-                        {categories[index].title}
-                    </MenuItem>
-                ))}
-            </Select>
-            <SelectedCategory category={selectValues} />
-        </div>
+        <>
+            {Object.keys(categories).map((category: string, index: number) => (
+                <GenreTreeView subgenres={categories[index]} />
+            ))}
+        </>
     )
 }
 
-interface Category {
-    title: string,
-    subGenre: Array<any>
-}
-
+/*
+return (
+    <div>
+        <Select name="category" onChange={handleChange} value={selectValues} multiple>
+            {Object.keys(categories).map((category: string, index: number) => (
+                <MenuItem key={index} value={categories[index].title} >
+                    {categories[index].title}
+                </MenuItem>
+            ))}
+        </Select>
+        <SelectedCategory category={selectValues} />
+    </div>
+)*/
 
