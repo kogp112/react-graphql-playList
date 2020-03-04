73691f9a4cfe3862c7eaabc7bfc1eb50b7208d94
kanami.ogawa <gapkana@gmail.com>
2020-03-04 23:46:54 +0900

    delete console.log

--- src/components/play-list/index.tsx
+++ src/components/play-list/index.tsx
@@ -22,14 +22,12 @@ const PlayListRow = (props) => {
 class PlayList extends React.Component<PropsType, {}> {
   public render() {
     if (this.props.list.length != 0) {
-      console.log('play list component props is ', this.props.list[0]['songs']);
-      //console.log('play list component props is ', this.props.list.length);
       return (
         <>
           <div>{this.props.list[0]['name'] ? this.props.list[0]['name'] : null}</div>
-            {this.props.list[0]['songs'] ? Object.keys(this.props.list[0]['songs']).map((value, index) => (
-              <PlayListRow children={this.props.list[0]['songs'][index]} />
-            )) : null}
+          {this.props.list[0]['songs'] ? Object.keys(this.props.list[0]['songs']).map((value, index) => (
+            <PlayListRow children={this.props.list[0]['songs'][index]} />
+          )) : null}
         </>
       );
     } else {
