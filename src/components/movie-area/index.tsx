496f3259f7d46da889da416040feb7abcd2e0db6
kanami.ogawa <gapkana@gmail.com>
2020-03-29 17:23:56 +0900

    change youtube url for emmbded

--- src/components/movie-area/index.tsx
+++ src/components/movie-area/index.tsx
@@ -4,8 +4,17 @@ interface PropsType {
   url: string;
 }
 
+function changeUrl(url: string) {
+  if (url !== undefined) {
+    const splitName = url.replace("watch?v=", "embed/");
+    console.log(splitName);
+    return splitName;
+  }
+  return;
+}
+
 export default function MovieArea(props: PropsType) {
   return (
-    <div><iframe width="500" height="400" src={props.url} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe></div>
+    <div><iframe width="500" height="400" src={changeUrl(props.url)} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe></div>
   );
-};
+}
