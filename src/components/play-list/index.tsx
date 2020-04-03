7996b290a94f869fe6a5e1b34bec4ef74b5ca8a6
kanami.ogawa <gapkana@gmail.com>
2020-04-03 21:46:51 +0900

    add React.Fragment for return multiple elements in play-list component

--- src/components/play-list/index.tsx
+++ src/components/play-list/index.tsx
@@ -5,45 +5,53 @@ import ButtonBase from "@material-ui/core/ButtonBase";
 
 interface PropsType {
   list: string[];
-  name: string;
-  handleClickPlayList: (arg0: any) => void;
+  name?: string;
+  handleClickPlayList: (event: string) => void;
 }
 
 const PlayListRow = (props: { onClick: (arg0: any) => void; children: { [x: string]: React.ReactNode; }; }) => {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={4}>
-        <ButtonBase onClick={(event) => {
-          props.onClick(props.children["url"]);
-          event.preventDefault();
-        }}>
-          <img className="musicImage" src={props.children["imageUrl"]} />
-        </ButtonBase>
+    <React.Fragment>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <ButtonBase onClick={(event) => {
+            props.onClick(props.children["url"]);
+            event.preventDefault();
+          }}>
+            <img className="musicImage" src={props.children["imageUrl"]} />
+          </ButtonBase>
+        </Grid>
+        <Grid item xs={7}>
+          <ButtonBase>
+            <p className="musicName">{props.children["name"]}</p>
+          </ButtonBase>
+        </Grid>
       </Grid>
-      <Grid item xs={7}>
-        <ButtonBase>
-          <p className="musicName">{props.children["name"]}</p>
-        </ButtonBase>
-      </Grid>
-    </Grid>
+    </React.Fragment>
   );
 };
 
 export default function PlayList(props: PropsType) {
   if (props.list.length !== 0) {
     return (
-      <Grid>
-        <Grid item>
-          <h3>{props.list[0]["name"] ? props.list[0]["name"] : null}</h3>
+      <React.Fragment>
+        <Grid>
+          <Grid item>
+            <h3>{props.list[0]["name"] ? props.list[0]["name"] : null}</h3>
+          </Grid>
+          <Grid item>
+            {props.list[0]["songs"] ? Object.keys(props.list[0]["songs"]).map((value, index) => (
+              <PlayListRow onClick={props.handleClickPlayList} children={props.list[0]["songs"][index]} />
+            )) : null}
+          </Grid>
         </Grid>
-        <Grid item>
-          {props.list[0]["songs"] ? Object.keys(props.list[0]["songs"]).map((value, index) => (
-            <PlayListRow onClick={props.handleClickPlayList} children={props.list[0]["songs"][index]} />
-          )) : null}
-        </Grid>
-      </Grid>
+      </React.Fragment>
     );
   } else {
-    return [];
+    return (
+      <React.Fragment>
+        <div>nothing</div>
+      </React.Fragment>
+    );
   }
 }
