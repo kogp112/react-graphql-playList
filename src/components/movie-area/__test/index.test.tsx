7b9f16b6149dd9629ba2487759f3bcd9c9ec7aa9
kanami.ogawa <gapkana@gmail.com>
2020-05-04 14:45:32 +0900

    modify unit test of movie-area

--- src/components/movie-area/__test/index.test.tsx
+++ src/components/movie-area/__test/index.test.tsx
@@ -1,23 +1,36 @@
-import React from "react";
+import * as React from "react";
 import { act } from "react-dom/test-utils";
-import { render, unmountComponentAtNode } from "react-dom";
+import { render } from "react-dom";
+import { expect } from "chai";
+import * as renderer from "react-test-renderer";
 import MovieArea from "../index";
 
-let container = null;
+let container: JSXElement = null;
+type JSXElement = Element | null;
+
 beforeEach(() => {
   container = document.createElement("div");
   document.body.appendChild(container);
 });
 
 afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
   container = null;
 });
 
-it('should render movie area', () => {
+it("should render movie area", () => {
   const url = "test";
   act(() => {
     render(<MovieArea url={url} />, container);
   });
+});
+
+it("convert url props correctlly", () => {
+  const url: string = "https://www.youtube.com/watch?v=test";
+  const component = renderer.create(
+    <MovieArea url={url} />,
+  );
+  const tree = component.toTree();
+  console.log(tree?.rendered?.props.children.props.src);
+  const expectedUrl = "https://www.youtube.com/embed/test";
+  expect(tree?.rendered?.props.children.props.src).to.equal(expectedUrl);
 });
